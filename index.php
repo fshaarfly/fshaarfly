ccb2567ee7454e02e264262b697775228763e818
fshaarfly <fasha.arrafly@gmail.com>
2025-01-22 04:47:45 +0700

    Update changes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sambatan Logo</title>
+    <title>Sambatan</title>
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
     <link rel="icon" type="image/x-icon" href="assets/sambatanlogo.png">
